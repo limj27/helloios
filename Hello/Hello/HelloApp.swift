17e57a0a5d86ac65d39dd69d6d72f12cbf371dae
limj27 <limjustin2@gmail.com>
2021-04-01 20:20:18 -0700

    Hello World modified, checked for build/emulator

---
+++ Hello/Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Justin Lim on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
